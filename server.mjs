ca0bf2622ecfb9a85a23423d91cad52a22f618a2
akshatmittal61 <akshatmittal2506@gmail.com>
2022-02-16 13:39:25 +0530

    connected db

--- server.mjs
+++ server.mjs
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
 const app = express();
+const connectDB = async () => {
+	try {
+		mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+		console.log("Connected to MongoDB successfuly");
+	} catch (err) {
+		console.log(err.message);
+		process.exit(1);
+	}
+};
+connectDB();
 
 app.get("/", (req, res) => {
 	res.send("Hello world");
