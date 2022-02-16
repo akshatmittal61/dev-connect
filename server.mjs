b9134ef4365463a4fc171fa4d7593b2f144e7613
akshatmittal61 <akshatmittal2506@gmail.com>
2022-02-16 14:30:34 +0530

    added all routes

--- server.mjs
+++ server.mjs
@@ -1,11 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import apiUsers from "./Routes/API/users.mjs";
+import apiAuth from "./Routes/API/auth.mjs";
+import apiProfile from "./Routes/API/profile.mjs";
+import apiPosts from "./Routes/API/posts.mjs";
 config();
 const app = express();
 const connectDB = async () => {
 	try {
-		mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+		await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
 		console.log("Connected to MongoDB successfuly");
 	} catch (err) {
 		console.log(err.message);
@@ -17,6 +21,11 @@ connectDB();
 app.get("/", (req, res) => {
 	res.send("Hello world");
 });
+
+app.use("/api/users", apiUsers);
+app.use("/api/auth", apiAuth);
+app.use("/api/profile", apiProfile);
+app.use("/api/posts", apiPosts);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
