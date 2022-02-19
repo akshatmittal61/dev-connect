70d680cf8a55946b7df920796505ef84f4cfb1b5
akshatmittal61 <akshatmittal2506@gmail.com>
2022-02-19 10:18:15 +0530

    delete post by id

--- Routes/API/posts.mjs
+++ Routes/API/posts.mjs
@@ -52,4 +52,20 @@ router.get("/:id", auth, async (req, res) => {
 	}
 });
 
+router.delete("/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) return res.status(404).json({ message: "Post not found" });
+		if (post.user.toString() !== req.user.id)
+			return res.status(401).json({ message: "User not authorized" });
+		await post.remove();
+		res.json({ message: "Post removed" });
+	} catch (err) {
+		console.error(err);
+		if (err.kind === "ObjectId")
+			return res.status(404).json({ message: "Post not found" });
+		res.status(500).send("Server Error");
+	}
+});
+
 export default router;
