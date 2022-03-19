02588464db4bb25f76ca34917ebb3e7f36481fd4
akshatmittal61 <akshatmittal2506@gmail.com>
2022-03-19 19:48:08 +0530

    added header check for auth token

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import { loadUser } from "./actions/auth";
+import store from "./store.js";
 
 const App = () => {
 	const location = useLocation();
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
 	return (
 		<>
 			<Navbar />
